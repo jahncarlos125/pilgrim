29e19f3eb15a523e9a07d135479996276ef48575
Jahn Carlos <jahn.santana@vortigo.com.br>
2021-07-04 15:47:42 -0300

    ♻️ Add alt-text in image logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/logo.png';
 
 export default function Header() {
   return (
     <div className='flex flex-row h-12 px-4 py-2 bg-pilgrim'>
-      <img src={Logo} className='h-10 w-auto'/>
+      <img src={Logo} alt='logo' className='h-10 w-auto'/>
     </div>
   )
 }
