87f144652ffb467f6b93ca19bca8acc14144a6d6
Jahn Carlos <jahn.santana@vortigo.com.br>
2021-07-04 22:32:29 -0300

    ✨ Add function "add to cart"

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -30,9 +30,12 @@ export default function ProductDetails(props: {selectedProduct: Product}) {
           <figure className='rounded-xl relative'>
             <img className='object-contain rounded-lg' src={cover_path} alt="" />
             {renderType(format_type)}
-            <div className="absolute -bottom-3 -right-3 bg-pilgrim p-1 text-white text-2xl md:text-3xl rounded-xl font-medium">
-              R$ 59,99
-            </div>
+            <button onClick={() => alert('Item adicionado ao carrinho!')} title='Adicionar ao carrinho' className="flex flex-row items-center absolute -bottom-3 -right-3 bg-pilgrim p-1 text-white text-2xl md:text-3xl rounded-xl font-medium">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-1 fill-current" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+              </svg>
+                R$ 59,99
+            </button>
           </figure>
         </div>
         <div className='flex flex-col flex-1 justify-center pt-2 md:ml-5'>
