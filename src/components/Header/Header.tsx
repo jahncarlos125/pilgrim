93a0daf2cd7bf8b6e4be963c4d45ff482646dc27
Jahn Carlos <jahn.santana@vortigo.com.br>
2021-07-04 22:21:05 -0300

    🔇 remove log

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { useRedirect } from '../../hooks';
 export default function Header() {
   const location = useLocation();
   const redirect = useRedirect();
-  console.log(location)
+
   return (
     <div className='flex flex-row justify-between h-12 px-4 py-2 bg-pilgrim'>
       <img src={Logo} alt='logo' className='h-10 w-auto'/>
